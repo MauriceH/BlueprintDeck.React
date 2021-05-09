25bd890098ac5e2eff83c51618c3613db20bf9f9
Maurice Hessing <m.hessing@validdata.de>
2021-05-09 14:41:10 +0200

    feat: display possible node connections (#10)
    refs #10

--- source/src/blueprint-deck/src/nodes/BaseNode.tsx
+++ source/src/blueprint-deck/src/nodes/BaseNode.tsx
@@ -5,6 +5,7 @@ import {NodePort} from "./NodePort";
 import {PortInputOutputType} from "../BluePrintRegistry";
 import "./BaseNode.css";
 import {useStoreState, Edge} from "react-flow-renderer";
+import {ElementId} from "react-flow-renderer/dist/types";
 
 
 export type BaseNodeProps =
@@ -16,6 +17,18 @@ export type BaseNodeProps =
 export const BaseNode: FC<BaseNodeProps> = ({node, children, ...divProps}) => {
 
     const selectedElements = useStoreState((store) => store.selectedElements);
+    const openConnection = useStoreState(store => {
+        return {handleId: store.connectionHandleId, handleType: store.connectionHandleType, nodeId: store.connectionNodeId}
+    });
+    const nodes = useStoreState(store => store.nodes);
+    const edges = useStoreState(store => store.edges);
+
+
+    const connectingNode = nodes.find(x=>x.id == openConnection?.nodeId) as BlueprintNodeData;
+    const connectingPort = connectingNode?.data?.ports?.find(x=>x.key == openConnection.handleId)
+    const inputEdges = edges.filter(x=>x.target == node.id)
+    const outputEdges = edges.filter(x=>x.source == node.id)
+
 
     const typeVisible = selectedElements?.find(x=>x.id == node.id) != null;
 
@@ -23,18 +36,30 @@ export const BaseNode: FC<BaseNodeProps> = ({node, children, ...divProps}) => {
         ?.filter((x) => x.inputOutputType == PortInputOutputType.Input)
         .map(port => {
             const nodeTypeVisible = typeVisible || selectedElements?.find(x => (x as Edge<NodeData>).target == node.id && (x as Edge<NodeData>).targetHandle == port.key) != null
+            const alreadyConnected = inputEdges.find(x=>x.targetHandle == port.key) != null
+            const displayType = connectingPort == null ? 'default' : (openConnection.handleType != "source" || connectingPort.typeId != port.typeId || alreadyConnected)  ? 'unConnectable' : 'suggestion';
+
             return <NodePort key={port.key}
                              port={port}
-                             typeVisible={nodeTypeVisible}/>;
+                             typeVisible={nodeTypeVisible}
+                             displayType={displayType}
+                             isConnectable={!alreadyConnected}
+            />;
         });
 
     const outputs = node.data?.ports
         ?.filter((x) => x.inputOutputType == PortInputOutputType.Output)
         .map(port => {
             const nodeTypeVisible = typeVisible || selectedElements?.find(x => (x as Edge<NodeData>).source == node.id && (x as Edge<NodeData>).sourceHandle == port.key) != null
+            const alreadyConnected = outputEdges.find(x=>x.sourceHandle == port.key) != null
+            const displayType = connectingPort == null ? 'default' : (openConnection.handleType != "target" || connectingPort.typeId != port.typeId || alreadyConnected)  ? 'unConnectable' : 'suggestion';
+
             return <NodePort key={port.key}
                              port={port}
-                             typeVisible={nodeTypeVisible}/>;
+                             typeVisible={nodeTypeVisible}
+                             displayType={displayType}
+                             isConnectable={!alreadyConnected}
+            />;
         });
 
     return (
--- source/src/blueprint-deck/src/nodes/NodePort.tsx
+++ source/src/blueprint-deck/src/nodes/NodePort.tsx
@@ -2,15 +2,18 @@ import {Handle, Position} from "react-flow-renderer";
 import React from "react";
 import {PortInputOutputType, RegistryNodePort} from "../BluePrintRegistry";
 import './NodePort.css'
+import './NodePortAnimation.css'
 import {HandleProps} from "react-flow-renderer/dist/types";
 import {useCheckValidConnection} from "./useCheckValidConnection";
 
 export type NodePortProps = {
     port: RegistryNodePort,
-    typeVisible?: boolean
+    typeVisible?: boolean,
+    displayType?: 'default' | 'suggestion' | 'unConnectable'
+    isConnectable?: boolean
 } & Partial<React.NamedExoticComponent<HandleProps & Omit<React.HTMLAttributes<HTMLDivElement>, "id">>>;
 
-export function NodePort({typeVisible, port, ...props}: NodePortProps) {
+export function NodePort({typeVisible, port, displayType, isConnectable, ...props}: NodePortProps) {
 
     const checkConnection = useCheckValidConnection(port);
 
@@ -21,21 +24,24 @@ export function NodePort({typeVisible, port, ...props}: NodePortProps) {
     const typeTitle = port.dataType?.title ?? 'Action';
     const portTitle = port.title ?? 'Action';
 
+    const handleClassName = displayType == "suggestion" ? 'port-suggestion' : displayType == "unConnectable" ? 'port-unconnectable' : '';
 
-    return <div>
-        {typeVisible && <div className={"port-type-container port-type-container-" + containerCssName}>
-          <div className={"port-type"}>{typeTitle}</div>
-        </div>}
-        <Handle
-            style={{position: "inherit"}}
-            id={port.key}
-            {...props}
-            type={handleType}
-            position={position}
-            isValidConnection={checkConnection}
-        />
-        <div className={"port-title-container port-title-container-" + containerCssName}>
-            <div className={"port-title"}>{portTitle}</div>
-        </div>
-    </div>;
+    return (
+        <div>
+            {typeVisible && <div className={"port-type-container port-type-container-" + containerCssName}>
+              <div className={"port-type"}>{typeTitle}</div>
+            </div>}
+            <Handle className={handleClassName}
+                    style={{position: "inherit"}}
+                    id={port.key}
+                    {...props}
+                    type={handleType}
+                    position={position}
+                    isValidConnection={checkConnection}
+                    isConnectable={isConnectable}
+            />
+            <div className={"port-title-container port-title-container-" + containerCssName}>
+                <div className={"port-title"}>{portTitle}</div>
+            </div>
+        </div>);
 }
